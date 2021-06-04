20213c3e35212b029ae7a8e426dae12f3c934afd
Emigdio Torres Ruiz <emigdio821@gmail.com>
2021-06-04 18:30:12 -0500

    added missing assets, completed pdf generator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,100 @@
-/*==================== SHOW MENU ====================*/
+// Show menu
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
 
-/*==================== REMOVE MENU MOBILE ====================*/
+//Remove mobile menu
+const navLink = document.querySelectorAll(".nav__link");
 
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+//Scroll section active link
+const sections = document.querySelectorAll("section[id]");
 
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-/*==================== SHOW SCROLL TOP ====================*/ 
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
 
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
 
-/*==================== DARK LIGHT THEME ====================*/ 
+// Scroll top button
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
 
+  this.scrollY >= 560
+    ? scrollTop.classList.add("show-scroll")
+    : scrollTop.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollTop);
 
-/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/ 
+// Dark/Light theme
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
 
-/*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/ 
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+});
 
+// PDF stuff
+function scaleCv() {
+  document.body.classList.add("scale-cv");
+}
 
-/*==================== GENERATE PDF ====================*/ 
-// PDF generated area
+function removeCvScale() {
+  document.body.classList.remove("scale-cv");
+}
 
+let pdfBtn = document.getElementById("pdf-button");
+let areaCvContainer = document.getElementById("area-cv");
 
-// Html2pdf options
+let pdfOpts = {
+  margin: 1,
+  filename: "emigdio-resume.pdf",
+  image: { type: "jpeg", quality: 0.98 },
+  html2canvas: { scale: 4 },
+  jsPDF: { format: "a4", orientation: "portrait" },
+};
 
+function genereatePdf() {
+  html2pdf(areaCvContainer, pdfOpts).then(() => {
+    removeCvScale();
+  });
+}
 
-// Function to call areaCv and Html2Pdf options 
-
-
-// When the button is clicked, it executes the three functions
-
-    // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-
-
-    // 2. The PDF is generated
-
-
-    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+pdfBtn.addEventListener("click", () => {
+  scaleCv();
+  genereatePdf();
+});
